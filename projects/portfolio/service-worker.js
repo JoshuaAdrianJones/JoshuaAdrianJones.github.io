7e2bda7f4c56f6be246ced093b5b2a6d1ffe8b3e
joshua <joshua.ar.jones@gmail.com>
2020-01-18 21:07:54 +0000

    trying to fix build

--- projects/portfolio/service-worker.js
+++ projects/portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f0206baceef90cfb4709d22374b9be5.js"
+  "/projects/portfolio/precache-manifest.ddb8ed1df3594a61e741c550b143906a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
